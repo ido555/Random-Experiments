ef716a23da914c221a15d694d3d9b1e9f702eaa7
ido555 <13142010+ido555@users.noreply.github.com>
2020-05-16 16:44:53 +0300

    a

--- Angular08052020/src/app/components/products/products.component.ts
+++ Angular08052020/src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MathService } from 'src/app/services/math.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +9,32 @@ import { MathService } from 'src/app/services/math.service';
 })
 export class ProductsComponent implements OnInit {
 
+  posts;
+
   // Dependency Injection 
-  constructor(private math: MathService) { }
+  constructor(private math: MathService, private productsSerice: ProductsService) { }
 
   ngOnInit(): void {
-    alert(this.math.isPrime(11))
-  }
 
+    // math service
+    console.log(this.math.isPrime(11))
+
+    // products service
+    // subscribe - asynchronous waiting for get request to complete   
+    // this.productsSerice.getPosts().subscribe(this.success, this.failure)
+    // lambda expression - function(){} turns into =>
+    this.productsSerice.getPosts().subscribe(
+      (response) => {
+        // console.log(response)
+        this.posts = response;
+      }, (err) => {
+        console.log(err)
+      })
+  }
+  // success(response) {
+  //   console.log(response)
+  // }
+  // failure(err) {
+  //   console.log(err)
+  // }
 }
--- Angular08052020/src/app/services/products.service.ts
+++ Angular08052020/src/app/services/products.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  // DI
+  constructor(private httpClient: HttpClient) { }
+
+  public getPosts() {
+    return this.httpClient.get("https://jsonplaceholder.typicode.com/posts")
+  }
 }
