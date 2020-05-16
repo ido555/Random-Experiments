d72ea4f3d4fbf419128f036af3e402fca218564d
ido555 <13142010+ido555@users.noreply.github.com>
2020-05-17 00:57:07 +0300

    a

--- ProductsServer16052020/src/main/java/com/example/controllers/ProductsController.java
+++ ProductsServer16052020/src/main/java/com/example/controllers/ProductsController.java
@@ -4,7 +4,11 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +16,28 @@ import com.example.beans.Product;
 
 @RestController
 @RequestMapping("products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductsController {
 	private List<Product> products = new ArrayList<Product>();
 
-	@GetMapping("getAll")
+	@GetMapping
 	public List<Product> getAllProducts() {
+		products.clear();
 		products.add(new Product("Laptop", 9000, new Date(System.currentTimeMillis())));
 		products.add(new Product("N95 Mask", 60, new Date(System.currentTimeMillis())));
 		products.add(new Product("Labradoodle", 1800, new Date(System.currentTimeMillis())));
 		products.add(new Product("Poodledoodle", 900, new Date(System.currentTimeMillis())));
 		return products;
+	}
+
+	// getOne/id
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getOneProduct(@PathVariable long id) {
+		for (Product product : products) {
+			if (product.getId() == id) {
+				return ResponseEntity.ok(product);
+			}
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product id not found");
 	}
 }
