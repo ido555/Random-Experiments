963a4c11d4af3f26f0f67c959afdcedaa1fa99db
ido555 <13142010+ido555@users.noreply.github.com>
2020-05-16 18:51:10 +0300

    a

---
+++ ProductsServer16052020/src/main/java/com/example/ProductsServer16052020Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServer16052020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServer16052020Application.class, args);
+	}
+
+}
---
+++ ProductsServer16052020/src/test/java/com/example/ProductsServer16052020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServer16052020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
