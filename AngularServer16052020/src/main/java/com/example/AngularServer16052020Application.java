1c3a5670f547ed4c22a3cf693aec9725acad4107
ido555 <13142010+ido555@users.noreply.github.com>
2020-05-17 17:51:17 +0300

    a

---
+++ AngularServer16052020/src/main/java/com/example/AngularServer16052020Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularServer16052020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularServer16052020Application.class, args);
+	}
+
+}
