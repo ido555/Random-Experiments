ce677016331644473d97bb3d87765c3df88b03bf
ido555 <13142010+ido555@users.noreply.github.com>
2020-05-30 00:40:40 +0300

    a

--- StocksProjectFrontEnd/src/app/app.component.ts
+++ StocksProjectFrontEnd/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+   
 })
-export class AppComponent {
-  title = 'StocksProjectFrontEnd';
+export class AppComponent implements OnInit {
+
+  // title = 'StocksProjectFrontEnd';
+  public primaryXAxis: Object;
+  public title: string;
+  public primaryYAxis: Object;
+  public data: Object[];
+  ngOnInit(): void {
+    this.data = [
+      { x: '1', open: 120, high: 160, low: 100, close: 140 },
+      { x: '2', open: 150, high: 190, low: 130, close: 170 },
+      { x: '3', open: 130, high: 170, low: 110, close: 150 },
+      { x: '4', open: 160, high: 180, low: 120, close: 140 },
+      { x: '5', open: 160, high: 180, low: 120, close: 140 },
+      { x: '6', open: 160, high: 180, low: 120, close: 140 },
+      { x: '7', open: 160, high: 180, low: 120, close: 140 },
+      { x: '8', open: 160, high: 180, low: 120, close: 140 },
+      { x: '9', open: 160, high: 180, low: 120, close: 140 },
+      { x: '10', open: 160, high: 180, low: 120, close: 140 },
+      { x: '11', open: 160, high: 180, low: 120, close: 140 },
+      { x: '12', open: 160, high: 180, low: 120, close: 140 },
+      { x: '13', open: 160, high: 180, low: 120, close: 140 },
+      { x: '14', open: 160, high: 180, low: 120, close: 140 },
+      { x: '15', open: 150, high: 170, low: 110, close: 130 }
+    ];
+    this.primaryXAxis = {
+      title: 'Date',
+      valueType: 'Category',
+    };
+    this.primaryYAxis = {
+      title: 'Price in Dollar', minimum: 100, maximum: 200, interval: 20,
+    };
+    this.title = 'Financial Analysis';
+  }
 }
--- StocksProjectFrontEnd/src/app/app.module.ts
+++ StocksProjectFrontEnd/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartModule } from '@syncfusion/ej2-angular-charts';
+import { CategoryService,CandleSeriesService } from '@syncfusion/ej2-angular-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
@@ -14,9 +15,11 @@ import { ChartComponent } from './components/chart/chart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule
   ],
-  providers: [],
+  
+  providers: [CategoryService,CandleSeriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
