2ee5b9334bc971431a8064794a990cc35a7fc5cd
ido555 <13142010+ido555@users.noreply.github.com>
2020-06-02 11:26:35 +0300

    a

--- AngularExamplesAndSite/src/app/app.component.spec.ts
+++ AngularExamplesAndSite/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular08052020'`, () => {
+  it(`should have as title 'AngularExamplesAndSite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular08052020');
+    expect(app.title).toEqual('AngularExamplesAndSite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular08052020 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularExamplesAndSite app is running!');
   });
 });
