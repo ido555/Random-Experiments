2554a42ce42175b552da20547911d33f7726d959
ido s <13142010+ido555@users.noreply.github.com>
2020-06-03 11:23:58 +0300

    a

--- AngularMaterial/src/app/app.component.ts
+++ AngularMaterial/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    options = [
+        {title:'Audi R8', color: 'red'},
+        {title:'Jaguar F-Type', color: 'black'}, 
+        {title:'Nissan GTR', color: 'SILVER'} ]
   title = 'AngularMaterial';
+  optionSelect: FormControl;
 }
--- AngularMaterial/src/app/app.module.ts
+++ AngularMaterial/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { MatBadgeModule } from "@angular/material/badge";
     MatSlideToggleModule,
     MatButtonModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatSelectModule,
+    MatFormFieldModule
     
   ],
   providers: [],
