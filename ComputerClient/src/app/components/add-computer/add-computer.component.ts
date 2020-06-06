cc2d55d3e0691a5df1d8c93f04fbef28ce2a6de8
ido555 <13142010+ido555@users.noreply.github.com>
2020-06-06 12:30:01 +0300

    a

---
+++ ComputerClient/src/app/components/add-computer/add-computer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-computer',
+  templateUrl: './add-computer.component.html',
+  styleUrls: ['./add-computer.component.css']
+})
+export class AddComputerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
