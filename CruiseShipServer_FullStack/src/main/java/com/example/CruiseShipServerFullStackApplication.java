2dbd9082cfd5c4565bd66510bee405aafad07420
ido555 <13142010+ido555@users.noreply.github.com>
2020-06-10 22:05:55 +0300

    a

---
+++ CruiseShipServer_FullStack/src/main/java/com/example/CruiseShipServerFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseShipServerFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiseShipServerFullStackApplication.class, args);
+	}
+
+}
