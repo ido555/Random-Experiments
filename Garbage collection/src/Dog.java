ba95f4223b2c017517e10b4a8939e0ca04d63578
ido555 <13142010+ido555@users.noreply.github.com>
2020-06-24 10:50:05 +0300

    a

---
+++ Garbage collection/src/Dog.java
@@ -0,0 +1,28 @@
+
+public class Dog {
+	private int age;
+	private String name;
+
+	public Dog(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	// special finalizer method
+	// runs right before garbage collection
+	// good for logs
+	public void finalize() {
+		System.out.println(name + " is being collected by GC");
+	}
+}
