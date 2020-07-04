355e174a532505654eff5765a3424c66428bd4b9
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-04 23:53:11 +0300

    a

---
+++ CouponsProject/src/app/models/admin.ts
@@ -0,0 +1,24 @@
+export class Admin {
+    constructor(
+        private _id: number
+    )
+}
+/*
+    constructor(
+        private _id: number, 
+        private _model:string, 
+        public ram:number,
+        public price:number){}
+
+    
+    get id() : number {
+        return this._id;
+    }
+
+    get model() : string{
+        return this._model;
+    }
+
+
+
+*/
