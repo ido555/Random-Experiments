2fa6c0cf05f89b74b5936d41053a5376efcd13f7
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-05 21:15:24 +0300

    a

--- CouponsProject/src/app/services/login-controller.service.ts
+++ CouponsProject/src/app/services/login-controller.service.ts
@@ -6,12 +6,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class LoginControllerService {
-
+  post:Object;
   constructor(private httpClient: HttpClient ) { }
   public login(clientType: ClientType, password: String, email: String) {
     // idk why httpParams:HttpParams) didnt work (missing provider or something) so i just skipped it
     // this.httpParams.append("email", email.toString());
     // this.httpParams.append("password", password.toString());
-    return this.httpClient.post("http://localhost:8080/login/" + clientType, { params: {"password":password.toString(), "email":email.toString()} })
+    this.post = {'email':email.toString(), "password":password.toString()};
+    return this.httpClient.post("http://localhost:8080/login/" + clientType, this.post, this.post)
   }
 }
