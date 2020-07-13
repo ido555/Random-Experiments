b4dca15191a20ff70a4f5e9129d3307150184887
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-14 01:05:27 +0300

    a

--- CouponsProject/src/app/components/admin/customer-controls/customer-controls.component.ts
+++ CouponsProject/src/app/components/admin/customer-controls/customer-controls.component.ts
@@ -52,20 +52,7 @@ export class CustomerControlsComponent implements OnInit {
       'right': 0,
       'bottom': 0,
     };
-
-    this.myParams = {
-      particles: {
-        number: {
-          value: 200,
-        },
-        color: {
-          value: '#ff0000'
-        },
-        shape: {
-          type: 'triangle',
-        },
-      }
-    };
+    this.myParams = this.reallyLongJson();
   }
   errPopup(err: string) {
     this.dialog.open(ErrorBoxComponent,
@@ -163,5 +150,70 @@ export class CustomerControlsComponent implements OnInit {
       subscribe(
         s => this.updateTable(s),
         e => this.errPopup(e.error))
+  }
+
+  reallyLongJson(){
+    return {
+      "particles": {
+        "number": {
+          "value": 100,
+          "density": {
+            "enable": true,
+            "value_area": 1000
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 4,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 1.5,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "bounce",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+    }
   }
 }
