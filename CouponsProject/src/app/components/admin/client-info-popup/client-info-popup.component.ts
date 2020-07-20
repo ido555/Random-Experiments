d567cd60475598c0bf976d30994ca953580eb59e
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-20 19:20:33 +0300

    a

--- CouponsProject/src/app/components/admin/client-info-popup/client-info-popup.component.ts
+++ CouponsProject/src/app/components/admin/client-info-popup/client-info-popup.component.ts
@@ -1,133 +1,104 @@
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
-import {Component, Inject, OnInit} from '@angular/core';
-import {ClientType} from 'src/app/enums/client-type.enum';
-import {AdminControllerService} from 'src/app/services/admin-controller.service';
-import {Company} from 'src/app/models/company';
-import {Customer} from 'src/app/models/customer';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {GlobalService} from '../../../services/global.service';
-
-@Component({
-  selector: 'app-client-info-popup',
-  templateUrl: './client-info-popup.component.html',
-  styleUrls: ['./client-info-popup.component.css']
-})
-export class ClientInfoPopupComponent implements OnInit {
-  err: any;
-  client;
-  comp: Company = null;
-  cust: Customer = null;
-  text: object;
-  // TODO fix? (make seperate classes for comp and cust) cant have just 1 clientForm for some reason - i tried for too long so i just moved on
-  compForm: FormGroup;
-  custForm: FormGroup;
-  ClientType = ClientType;
-
-  constructor(private dialogRef: MatDialogRef<ClientInfoPopupComponent>, @Inject(MAT_DIALOG_DATA) public data,
-              private cont: AdminControllerService, private dialog: MatDialog, private fb: FormBuilder, public glob: GlobalService) {
-  }
-
-  ngOnInit(): void {
-    this.client = this.data;
-    console.log(this.client);
-    if (this.client.companyId != null) {
-      this.comp = new Company(this.client.companyId, this.client.password, this.client.name, this.client.email);
-    }
-    if (this.client.customerId != null) {
-      this.cust = new Customer(this.client.customerId, this.client.password, this.client.email,
-        this.client.firstName, this.client.lastName, []);
-    }
-
-    if (this.comp != null) {
-      this.compForm = this.fb.group({
-        password: [this.comp.$password, Validators.required],
-        email: [this.comp.$email, [Validators.required, Validators.email]],
-        name: [this.comp.$name, Validators.required],
-      });
-    }
-    if (this.cust != null) {
-      this.custForm = this.fb.group({
-        firstName: [this.cust.$firstName],
-        lastName: [this.cust.$lastName],
-        password: [this.cust.$password, [Validators.required]],
-        email: [this.cust.$email, [Validators.required, Validators.email]]
-      });
-    }
-  }
-
-  closeDialog() {
-    this.dialogRef.close();
-  }
-
-  errPopup(e: string) {
-    this.glob.errPopup(e);
-  }
-
-  update(type: ClientType) {
-    this.updateClients();
-    if (type === ClientType.Company) {
-      this.updateCompany(this.comp);
-    }
-    if (type === ClientType.Customer) {
-      this.updateCustomer(this.cust);
-    }
-  }
-
-  updateClients() {
-    if (this.cust != null) {
-      if (this.custForm.valid) {
-        this.cust.$firstName = this.custForm.controls.firstName.value;
-        this.cust.$lastName = this.custForm.controls.lastName.value;
-        this.cust.$email = this.custForm.controls.email.value;
-        this.cust.$password = this.custForm.controls.password.value;
-      }
-    }
-    if (this.comp != null) {
-      if (this.compForm.valid) {
-        this.comp.$name = this.compForm.controls.name.value;
-        this.comp.$email = this.compForm.controls.email.value;
-        this.comp.$password = this.compForm.controls.password.value;
-      }
-    }
-  }
-
-  updateCustomer(cust: Customer) {
-    console.log(this.cust);
-    this.cont.updateCustomer(sessionStorage.getItem('token'), cust).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e));
-  }
-
-  updateCompany(comp: Company) {
-    this.cont.updateCompany(sessionStorage.getItem('token'), comp).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e));
-  }
-
-  deleteCustomer(customerId: number) {
-    console.log(this.custForm);
-    this.cont.deleteCustomer(sessionStorage.getItem('token'), customerId).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e));
-  }
-
-  deleteCompany(companyId: number) {
-    this.cont.deleteCompany(sessionStorage.getItem('token'), companyId).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e));
-  }
-
-  addCustomer() {
-    this.updateClients();
-    this.cont.addCustomer(sessionStorage.getItem('token'), this.cust).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e.error));
-  }
-
-  addCompany() {
-    this.updateClients();
-    this.cont.addCompany(sessionStorage.getItem('token'), this.comp).subscribe(
-      () => this.closeDialog(),
-      e => this.errPopup(e.error));
-  }
-}
+// import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+// import {Component, Inject, OnInit} from '@angular/core';
+// import {ClientType} from 'src/app/enums/client-type.enum';
+// import {AdminControllerService} from 'src/app/services/admin-controller.service';
+// import {Company} from 'src/app/models/company';
+// import {Customer} from 'src/app/models/customer';
+// import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import {GlobalService} from '../../../services/global.service';
+//
+// @Component({
+//   selector: 'app-client-info-popup',
+//   templateUrl: './client-info-popup.component.html',
+//   styleUrls: ['./client-info-popup.component.css']
+// })
+// export class ClientInfoPopupComponent implements OnInit {
+//   err: any;
+//   client;
+//   comp: Company = null;
+//   cust: Customer = null;
+//   text: object;
+//   // TODO fix? (make seperate classes for comp and cust) cant have just 1 clientForm for some reason - i tried for too long so i just moved on
+//   compForm: FormGroup;
+//   custForm: FormGroup;
+//   ClientType = ClientType;
+//
+//   constructor(private dialogRef: MatDialogRef<ClientInfoPopupComponent>, @Inject(MAT_DIALOG_DATA) public data,
+//               private cont: AdminControllerService, private dialog: MatDialog, private fb: FormBuilder, public glob: GlobalService) {
+//   }
+//
+//   ngOnInit(): void {
+//     this.client = this.data;
+//     console.log(this.client);
+//     if (this.client.companyId != null) {
+//       this.comp = new Company(this.client.companyId, this.client.password, this.client.name, this.client.email);
+//     }
+//     if (this.client.customerId != null) {
+//       this.cust = new Customer(this.client.customerId, this.client.password, this.client.email,
+//         this.client.firstName, this.client.lastName, []);
+//     }
+//
+//     if (this.comp != null) {
+//       this.compForm = this.fb.group({
+//         password: [this.comp.$password, Validators.required],
+//         email: [this.comp.$email, [Validators.required, Validators.email]],
+//         name: [this.comp.$name, Validators.required],
+//       });
+//     }
+//     if (this.cust != null) {
+//       this.custForm = this.fb.group({
+//         firstName: [this.cust.$firstName],
+//         lastName: [this.cust.$lastName],
+//         password: [this.cust.$password, [Validators.required]],
+//         email: [this.cust.$email, [Validators.required, Validators.email]]
+//       });
+//     }
+//   }
+//
+//   closeDialog() {
+//     this.dialogRef.close();
+//   }
+//
+//   errPopup(e: string) {
+//     this.glob.errPopup(e);
+//   }
+//
+//   update(type: ClientType) {
+//     this.updateClients();
+//     if (type === ClientType.Company) {
+//       this.updateCompany(this.comp);
+//     }
+//     if (type === ClientType.Customer) {
+//       this.updateCustomer(this.cust);
+//     }
+//   }
+//
+//   updateClients() {
+//     if (this.comp != null) {
+//       if (this.compForm.valid) {
+//         this.comp.$name = this.compForm.controls.name.value;
+//         this.comp.$email = this.compForm.controls.email.value;
+//         this.comp.$password = this.compForm.controls.password.value;
+//       }
+//     }
+//   }
+//   updateCompany(comp: Company) {
+//     this.cont.updateCompany(sessionStorage.getItem('token'), comp).subscribe(
+//       () => this.closeDialog(),
+//       e => this.errPopup(e));
+//   }
+//
+//
+//
+//   deleteCompany(companyId: number) {
+//     this.cont.deleteCompany(sessionStorage.getItem('token'), companyId).subscribe(
+//       () => this.closeDialog(),
+//       e => this.errPopup(e));
+//   }
+//   addCompany() {
+//     this.updateClients();
+//     this.cont.addCompany(sessionStorage.getItem('token'), this.comp).subscribe(
+//       () => this.closeDialog(),
+//       e => this.errPopup(e.error));
+//   }
+// }
--- CouponsProject/src/app/components/admin/customer-add-update-delete/customer-add-update-delete.component.ts
+++ CouponsProject/src/app/components/admin/customer-add-update-delete/customer-add-update-delete.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {AdminControllerService} from '../../../services/admin-controller.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {GlobalService} from '../../../services/global.service';
+import {Customer} from '../../../models/customer';
 
 @Component({
   selector: 'app-customer-add-update-delete',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add-update-delete.component.css']
 })
 export class CustomerAddUpdateDeleteComponent implements OnInit {
+  err: any;
+  cust: Customer;
+  text: object;
+  // TODO fix? (make seperate classes for comp and cust) cant have just 1 clientForm for some reason - i tried for too long so i just moved on
+  custForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dialogRef: MatDialogRef<CustomerAddUpdateDeleteComponent>, @Inject(MAT_DIALOG_DATA) public data,
+              private cont: AdminControllerService, private dialog: MatDialog, private fb: FormBuilder, public glob: GlobalService) {
   }
 
+  ngOnInit(): void {
+    this.cust = new Customer(this.client.customerId, this.client.password, this.client.email,
+      this.client.firstName, this.client.lastName, []);
+
+    this.custForm = this.fb.group({
+      firstName: [this.cust.$firstName],
+      lastName: [this.cust.$lastName],
+      password: [this.cust.$password, [Validators.required]],
+      email: [this.cust.$email, [Validators.required, Validators.email]]
+    });
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  errPopup(e: string) {
+    this.glob.errPopup(e);
+  }
+
+  refreshCustomer() {
+    this.cust.$firstName = this.custForm.controls.firstName.value;
+    this.cust.$lastName = this.custForm.controls.lastName.value;
+    this.cust.$email = this.custForm.controls.email.value;
+    this.cust.$password = this.custForm.controls.password.value;
+  }
+  addCustomer() {
+    refreshCustomer()
+    this.updateClients();
+    this.cont.addCustomer(sessionStorage.getItem('token'), this.cust).subscribe(
+      () => this.closeDialog(),
+      e => this.errPopup(e.error));
+  }
+  updateCustomer() {
+    refreshCustomer()
+    this.cont.updateCustomer(sessionStorage.getItem('token'), this.cust).subscribe(
+      () => this.closeDialog(),
+      e => this.errPopup(e));
+  }
+  deleteCustomer() {
+    this.cont.deleteCustomer(sessionStorage.getItem('token'), this.cust.$customerId).subscribe(
+      () => this.closeDialog(),
+      e => this.errPopup(e));
+  }
 }
