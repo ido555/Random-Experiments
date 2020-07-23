70ed58cdfb3d36529d283c0d142d0b33bf60b614
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-23 22:17:29 +0300

    a

--- CouponsProject/src/app/components/navbar/navbar.component.ts
+++ CouponsProject/src/app/components/navbar/navbar.component.ts
@@ -11,18 +11,13 @@ export class NavbarComponent implements OnInit {
   clientType: String;
   isLogged : boolean = this.glob.isLogged();
 
-  constructor(private logMan: LoginControllerService, public glob: GlobalService, private changeDecRef: ChangeDetectorRef) {
+  constructor(private logMan: LoginControllerService, public glob: GlobalService, private cd: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
-    // had do this since some *NgIf conditions weren't updating
-    setInterval(this.updateIf , 1000)
   }
-  updateIf(){
-    if (this.glob.isLogged())
-      this.isLogged = true;
-    this.isLogged = false;
-  }
+
+
   public logOut() {
     console.log(this.logMan);
     this.logMan.logout(sessionStorage.getItem('token')).subscribe(
