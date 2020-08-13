1382b16342c84ce24228b1b1e2eea4e0767ae789
ido555 <13142010+ido555@users.noreply.github.com>
2020-08-13 18:33:49 +0300

    a

--- Python/OpenGL_Cube.py
+++ Python/OpenGL_Cube.py
@@ -1,6 +1,7 @@
 import pygame
 
 # OpenGL.GL, the core GL library and extensions to it
+
 import OpenGL.GL
 
 # GLU is the OpenGL Utility Library.
@@ -9,44 +10,65 @@ import OpenGL.GL
 # and draw quadric surfaces and NURBs (Non-uniform rational B-spline) (not gonna pretend i know what this means...)
 # (Non-uniform rational basis spline is a mathematical model commonly
 # used in computer graphics for generating and representing curves and surfaces)
+
 import OpenGL.GLU
 
 # (x,y,z) coordinates of each vertex
 # edges will be drawn between 2 vertices
+# OpenGL uses a right-handed system
+# so +x = right
+# +y = up
+# +z = closer
 vertices = (
-    # top 4 vertices
+
+    # right bottom far vertex
     (1, -1, -1),
+    # right top far vertex
     (1, 1, -1),
+
+    # left top far vertex
     (-1, 1, -1),
+    # left bottom far vertex
     (-1, -1, -1),
 
-    # bottom 4 vertices
+
+    # right bottom close vertex
     (1, -1, 1),
+    # right top close vertex
     (1, 1, 1),
+
+    # left bottom close vertex
     (-1, -1, 1),
+    # left top close vertex
     (-1, 1, 1)
-    )
+)
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
 
-# determine the limits or boundaries of
 
 def Cube():
-    glBegin(GL_LINES)
+    gl = OpenGL.GL
+    # glBegin - determine the limits or boundaries of primitive or a group of like primitives
+    # Begin GL geometry-definition mode, disable automatic error checking
+    # ( define new 3d shape )
+    gl.glBegin(gl.GL_LINES)
+
     for edge in edges:
         for vertex in edge:
-            glVertex3fv(vertices[vertex])
-    glEnd()
+            gl.glVertex3fv(vertices[vertex])
+
+    # Finish GL geometry-definition mode, re-enable automatic error checking
+    gl.glEnd()
