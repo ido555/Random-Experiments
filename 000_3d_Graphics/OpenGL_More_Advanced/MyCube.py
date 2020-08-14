ac0d28d78e9d522ef02a782b4f2993b4b326f77a
ido555 <13142010+ido555@users.noreply.github.com>
2020-08-14 22:26:46 +0300

    a

--- 000_3d_Graphics/OpenGL_More_Advanced/MyCube.py
+++ 000_3d_Graphics/OpenGL_More_Advanced/MyCube.py
@@ -3,7 +3,8 @@
 # Height = 2 units
 # Volume = 8 units
 class Cube:
-    # static variables
+
+    # class variables
     vertices = ((1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
                 (1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1))
     edges = ((0, 1), (0, 3), (0, 4), (2, 1),
@@ -12,17 +13,21 @@ class Cube:
     quads = ((0, 1, 2, 3), (3, 2, 7, 6), (6, 7, 5, 4),
              (4, 5, 1, 0), (1, 5, 7, 2), (4, 0, 3, 6))
 
-    def __init__(self):
+    # create instance and set initial location in relation to world origin
+    def __init__(self, x, y, z):
         self.newVertices = []
+        self.set_location(x, y, z)
 
+    # set location in relation to world origin
     def set_location(self, x, y, z):
+        # get dimensions of a 2x2x2
         for vertex in self.vertices:
             old_x = Cube.vertices[vertex][0]
             old_y = Cube.vertices[vertex][1]
             old_z = Cube.vertices[vertex][2]
-            oldVertex = Vertex(old_x, old_y, old_z)
-
-            self.newVertices.append(Vertex(oldVertex.x + x, oldVertex.y + y, oldVertex.z + z))
+            old_vertex = Vertex(old_x, old_y, old_z)
+            # move the whole cube without distorting its shape
+            self.newVertices.append(Vertex(old_vertex.x + x, old_vertex.y + y, old_vertex.z + z))
 
 
 class Vertex:
--- 000_3d_Graphics/OpenGL_More_Advanced/OpenGL_FPS-Like.py
+++ 000_3d_Graphics/OpenGL_More_Advanced/OpenGL_FPS-Like.py
@@ -2,3 +2,25 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import random
+import OpenGL_More_Advanced.MyCube as MyCube
+
+
+def main():
+
+    def render_cube(cube):
+        glBegin(GL_LINES)
+        for edge in cube.edges:
+            for vertex in edge:
+                glVertex3fv(cube.newVertices[vertex])
+        glEnd()
+
+        glBegin(GL_QUADS)
+        for quad in cube.quads:
+            for vertex in quad:
+                glVertex3fv(cube.newVertices[vertex])
+        glEnd()
+
+
+
+main()
