a5be3a1f51c35e8bb9a088c67878487d89569826
ido555 <13142010+ido555@users.noreply.github.com>
2020-08-30 21:00:58 +0300

    a

--- word permutations.py
+++ word permutations.py
@@ -1,7 +1,35 @@
-from itertools import permutations
+from itertools import permutations as pe
+import multiprocessing
 
-permu = [''.join(p) for p in permutations('stack')]
-permu = set(permu)
+# TODO make this work multi-threaded
+def main():
+    threadPool = multiprocessing.Pool()
 
-with open("permus.txt", "w") as output:
-    output.write(str(values))
+    # decipher "syollrdecean" by brute force
+
+    permutations = [''.join(p) for p in pe('abcd')]
+
+    # python uses latin-1 encoding in this case for optimization = 1 byte per char
+    # "syollrdecean" should take up 479 Megabytes in ram
+
+    # O(n!) - factorial time complexity
+
+    with open("permus.txt", "w") as output:
+        # up to 8 threads
+        permutations = threadPool.map(calcPermus(permutations), range(8))
+        for permutation in permutations():
+            output.write(permutation)
+
+
+def calcPermus(permutationsList):
+    filteredPermutations = []
+    for permu in permutationsList:
+        firstThree = permu[:2]
+        theRest = permu[2:]
+        permu = firstThree + "\t" + theRest
+        filteredPermutations.append(permu + "\n")
+    return filteredPermutations
+
+
+if __name__ == '__main__':
+    main()
