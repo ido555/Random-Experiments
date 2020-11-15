3a799adc58a2f7cbcb2446b48236c488c4f44432
ido555 <13142010+ido555@users.noreply.github.com>
2020-11-15 04:06:20 +0200

    Update NNFS softmax activation_normalization_exponentiation_euler's number.py

--- 002_Neural Networks/NNFS softmax activation_normalization_exponentiation_euler's number.py
+++ 002_Neural Networks/NNFS softmax activation_normalization_exponentiation_euler's number.py
@@ -36,8 +36,20 @@ print('Sum of normalized values:', np.sum(normValues))
 
 # normalize for each sample
 # probabilities = expValues / np.sum(expValues, axis=1, keepdims=True)
+# what axis and keepDims do:
+'''
+using this for example
+np.array(
+[[4.8, 1.21, 2.385],
+[8.9, -1.81, 0.2],
+[1.41, 1.051, 0.026]])
+'''
 
 
+# sum with axis=None - 18.172 - sums everything together
+# axis=0 - sums rows - [15.11 0.451 2.611]
+# axis=1 - sums columns - [8.395  7.29  2.487]
+# axis=1 and keepDims=True - [[8.395]  [7.29]  [2.487]]
 
 # without numpy
 # For each value in a vector, calculate the exponential value
@@ -56,3 +68,17 @@ print('Sum of normalized values:', np.sum(normValues))
 # print('Sum of normalized values:', sum(normValues))
 
 
+# and finally, softmax activation summed up as a class
+# remember softmax activation can be a source of exploding magnitudes in numbers
+class ActivationSoftmax:
+    # Forward pass
+    def forward(self, inputs):
+        # Get unnormalized probabilities
+        # using inputs - np.max to get rid of the largest number in the input data
+        # to help with numbers exploding in size and also with buffer overflow
+        # due to the (awesome)magic of math which i dont fully understand this wont change the output
+        expValues = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        # Normalize them for each sample
+        probabilities = expValues / np.sum(expValues, axis=1, keepdims=True)
+        self.output = probabilities
