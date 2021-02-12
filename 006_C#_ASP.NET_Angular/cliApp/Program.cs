540d8da9e8e3eff2f8020596082fbcb8f402f1fd
ido555 <13142010+ido555@users.noreply.github.com>
2021-02-12 03:28:58 +0200

    a

---
+++ 006_C#_ASP.NET_Angular/cliApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace cliApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test(1 , 5);
+            Console.WriteLine(t.ToString());
+            Console.WriteLine(t.);
+        }
+    }
+}
